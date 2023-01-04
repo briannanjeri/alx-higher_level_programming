bb957102fd89f2c3ffbbadafd1da5ebb5727c93d
briannanjeri <briannanjeri2020@gmail.com>
2023-01-04 10:54:06 +0300

    initial commit

--- 0x0B-python-input_output/6-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""From JSON string to object module"""
-
-
-def from_json_string(my_str):
-    """Returns an object represented by a JSON string"""
-    import json
-    return json.loads(my_str)
