d715a1c38577add237d0d3968a457553c81581a7
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-13 12:12:54 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-  }
+    constructor(w,h){
+        if(w>0 && h>0){
+            [this.width, this.height]=[w,h]
+        }
+    }
+ print(){
+    for(let i=0; i<this.height; i++){
+        let row=""
+        for(let j=0; j<=this.width; j++){
+            row+="x"
+        }
+         console.log(row)
+    }
+   
+}
 
-  print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-  }
-};
+}
