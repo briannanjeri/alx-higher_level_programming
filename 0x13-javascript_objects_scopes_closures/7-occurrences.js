349532c3877ecc6e799051f5e0bfbde1b10b165e
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-14 10:26:36 +0300

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let count=0;
-   for(let i=0; i<list.length; i++){
-       if(list[i]==searchElement){count++}
-   }
-    return(count)
-};
+  return list.reduce((count, currentelement) => currentelement === searchElement ? count + 1 : count, 0);
