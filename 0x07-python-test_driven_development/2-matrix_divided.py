5b7ee48cd4791634c851c09bd4ff912037820b51
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-22 21:51:44 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,9 +22,9 @@ def matrix_divided(matrix, div):
         for i in l:
             if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of \integers/floats")
-        if type(div) is not int and type(div) is not float:
-            raise TypeError("div must be a number")
-        if div == 0:
-            raise ZeroDivisionError("division by zero")
-        return [[round(i / div, 2) for i in l] for l in matrix]            
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in l] for l in matrix]            
 
