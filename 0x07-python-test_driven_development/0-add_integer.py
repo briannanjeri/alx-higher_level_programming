777a1bcc2d82001e9815a488e415373db22a1f89
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-22 19:07:17 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This is an integer additon function.
+It takes 2 arguments and return the results
+"""
 
 def add_integer(a, b):
     """Return the addition of two numbers."""
