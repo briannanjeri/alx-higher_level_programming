a100fba16019954bf15e91d63b34827d657aeda5
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-22 11:37:48 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -21,7 +21,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-     """set the current size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
