327d7a1915a71c7e2f33b9aa72d48b4023048283
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-26 17:53:32 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ import sys
 if __name__=="__main__":
     Base=declarative_base()
     engine=create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                         format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                         format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
  """define the state model"""  
 class State(Base):
     __tablename__ = 'states'
@@ -28,5 +28,4 @@ states=session.query(State).order_by(State.id.asc()).all()
 for state in states:
     print("({}, '{}')".format(state.id, state.name))
 
-"""close the session"""
 session.close()   
