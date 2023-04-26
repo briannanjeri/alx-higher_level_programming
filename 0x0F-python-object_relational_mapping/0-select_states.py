163e172fdfc1a58039758392e8c2b21b28673bb0
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-26 18:02:37 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ This script retrieves all states from a MySQL database and prints them to the te
 Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
 """
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
 import sys
@@ -14,14 +14,14 @@ if __name__=="__main__":
     Base=declarative_base()
     engine=create_engine("mysql+mysqldb://{}:{}@localhost/{}".
                          format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
  """define the state model"""  
 class State(Base):
     __tablename__ = 'states'
     id=Column(Integer, primary_key=True, nullable=False) 
     name=Column(String(128), nullable=False)   
 """create a session to interact with database"""
-Session=sessionmaker(bind=engine)
-session=Session()
 """query all states and print their names"""
 states=session.query(State).order_by(State.id.asc()).all()
 
