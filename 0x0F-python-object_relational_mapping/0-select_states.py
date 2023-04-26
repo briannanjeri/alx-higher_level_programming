bdb1af95a6cd1418ec7322bea3d1d1d735b26e6a
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-26 18:21:29 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-
-"""
-Script that lists all states from the database hbtn_0e_0_usa
-"""
-
-import MySQLdb
+'''
+lists all states from the database hbtn_0e_0_usa
+'''
 import sys
+import MySQLdb
 
-# Connect to the MySQL database
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
 
-# Create a cursor object
-cur = db.cursor()
-
-# Execute the query to retrieve all states from the database
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-# Fetch all the rows and print them
-for row in cur.fetchall():
-    print(row)
-
-# Close the cursor and database connection
-cur.close()
-db.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for state in query_rows:
+      print("({}, '{}')".format(state.id, state.name))
+    cur.close()
+    conn.close()
