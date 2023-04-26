9f9cb42bc2babdbd2c5ace75e8a542d034779d54
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-26 18:06:53 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,28 +4,35 @@ This script retrieves all states from a MySQL database and prints them to the te
 
 Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
 """
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
-if __name__=="__main__":
-    Base=declarative_base()
-    engine=create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                         format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
- """define the state model"""  
-class State(Base):
-    __tablename__ = 'states'
-    id=Column(Integer, primary_key=True, nullable=False) 
-    name=Column(String(128), nullable=False)   
-"""create a session to interact with database"""
-"""query all states and print their names"""
-states=session.query(State).order_by(State.id.asc()).all()
 
-for state in states:
-    print("({}, '{}')".format(state.id, state.name))
+if __name__ == '__main__':
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-session.close()   
+    # Create connection to database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(mysql_username, mysql_password, database_name),
+                           pool_pre_ping=True)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Get all states and print them
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()   
