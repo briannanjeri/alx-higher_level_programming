cb8b2f6028b14ad764e0a83d3eb7a49adb8e0e8f
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-04-27 18:12:51 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,9 +10,9 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]),pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
