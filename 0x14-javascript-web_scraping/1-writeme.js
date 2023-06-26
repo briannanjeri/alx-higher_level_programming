d8eb3c94dfe438b23a31f3c0ee3f0651d86f095d
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-06-26 08:37:23 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-// Writes a string to a file.
 const fs = require('fs');
-const fileName = process.argv[2];
-const fileText = process.argv[3];
-
-fs.writeFile(fileName, fileText, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
