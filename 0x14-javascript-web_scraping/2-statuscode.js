121951b35cf5cc7dfec17c9d3fc6686d80758bc2
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-06-26 08:38:33 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-// Displays the status code of a GET request.
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  const status = response.statusCode;
-  console.log('code: ' + status);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
