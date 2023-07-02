bbe82ec77f95e52f3e011b7a21afe9d3a3a09ae3
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-07-02 13:43:41 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 // Prints 3 lines
 
-const firstLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'Javascript is amazing';
-
-console.log(firstLine + '\n' + secondLine + '\n' +
-                thirdLine);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
