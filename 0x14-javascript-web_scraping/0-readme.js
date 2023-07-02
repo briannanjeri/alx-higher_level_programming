bba25dcbbb06163e9a2bd7ae247b9ab8d641ad30
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-07-02 12:56:18 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-// Reads and prints the content of a file.
-import sys
-
-def read_and_print_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content, end='')
-    except Exception as e:
-        print(e)
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <file_path>")
-    else:
-        file_path = sys.argv[1]
-        read_and_print_file(file_path)
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
