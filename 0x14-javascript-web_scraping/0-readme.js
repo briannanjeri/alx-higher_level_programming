d4fe3ce71c5b1271dc67754e6a319b46a751c961
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2023-09-26 19:16:05 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+// Reads and prints the content of a file
 
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
