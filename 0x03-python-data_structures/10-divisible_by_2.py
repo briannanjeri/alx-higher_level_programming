112940c2a46e01343e1f493aa3783796c59018d2
briannanjeri <briannanjeri2020@gmail.com>
2024-08-13 13:32:04 +0300

    modified hello word folder to edit print numbers and float file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-# 10-divisible_by_2.py
-# divisible by 2
-
-
 def divisible_by_2(my_list=[]):
-    """Find all multiples of 2 in a list."""
-    newlist = []
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            newlist.append(True)
+            new_list.append(True)
         else:
-            newlist.append(False)
-
-    return (newlist)
+            new_list.append(False)
+    return new_list
