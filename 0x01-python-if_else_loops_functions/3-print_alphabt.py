451531c1a34922a81a6ce6b0dd82999ead857f8b
briannanjeri <briannanjeri2020@gmail.com>
2024-08-13 13:40:55 +0300

    modified print alphabt and tebahpla to provide correct output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-   if chr(i) not in ['e','q']:
-      print("{}".format(chr(i)), end="")
+# Author - Godswill Kalu
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
