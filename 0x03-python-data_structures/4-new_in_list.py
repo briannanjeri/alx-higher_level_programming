cb407b2b76b802b039eb17c75a07a147254ef3d8
briannanjeri <briannanjeri2020@gmail.com>
2024-08-13 13:59:52 +0300

    modified print matrix and new list files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# 4-new_in_list.py
-#nanjeribrian
-
-
 def new_in_list(my_list, idx, element):
-    """Replace an element in a copied list at a specific position."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    newlist = [x for x in my_list]
-    newlist[idx] = element
-    return (newlist)
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
